d4b0a029e3603cd4ef3f3c7dfe0c460eadd17850
camilistarosky <camili.naara@gmail.com>
2024-05-28 20:08:58 -0300

    Anatomia da classe

---
+++ bootcamp/src/edu/camili/anatomiaClasse/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.camili.anatomiaClasse;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 3;
+
+        if (mediaFinal >= 6) {
+            System.out.println("APROVADO!");
+        } else {
+            System.out.println("REPROVADO!");
+        }
+    }
+}
---
+++ bootcamp/src/edu/camili/anatomiaClasse/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.camili.anatomiaClasse;
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+        String primeiroNome = "Camili";
+        String segundoNome = "Starosky";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    private static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
